726b70eff99793e936f0943109b4a565af39a7fe
kathrynsiegel <ksiegel@mit.edu>
2015-10-20 23:06:15 -0400

    initial skeleton for LRModel

---
+++ src/main/scala/edu/mit/csail/db/ml/LRModel.scala
@@ -0,0 +1,27 @@
+package edu.mit.csail.db.ml
+
+import java.io.OutputStream
+import org.apache.spark.sql.DataFrame
+
+class LRModel extends Model {
+  /**
+   * Predict target variables using this model.
+   * By default, the output data frame should contain an output column called "prediction".
+   * This should be overridden in subclasses and configurable by the user.
+   * @param data
+   * @return
+   */
+  def predict(data: DataFrame): DataFrame {
+    // TODO: predict based on model
+    // y = (alpha * x + beta + error > 0) ? 1 : 0
+  }
+
+  /**
+   * Export model to a different format
+   * @param format
+   * @param out
+   */
+  def export(format: String, out:OutputStream) {
+    // TODO
+  }
+}
