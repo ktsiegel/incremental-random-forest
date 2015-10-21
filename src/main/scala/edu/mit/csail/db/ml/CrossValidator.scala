78f2137fb4f1d48883ffdc0fea6a15ca4a58fa4f
kathrynsiegel <ksiegel@mit.edu>
2015-10-20 22:33:51 -0400

    ported over cv code from estimator branch; this is still a work in progress

--- src/main/scala/edu/mit/csail/db/ml/CrossValidator.scala
+++ src/main/scala/edu/mit/csail/db/ml/CrossValidator.scala
@@ -2,26 +2,48 @@ package edu.mit.csail.db.ml
 
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.sql.DataFrame
+import edu.mit.csail.db.ml.{Model, ModelSpec, Evaluator}
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.tuning.{CrossValidator => SparkCrossValidator}
 
 /**
  * This class is responsible for performing cross validation given a set of parameters
  * and models
  * Created by mvartak on 10/14/15.
  */
-class CrossValidator ( // TODO: should this extend estimator similar to spark.ml?
+class CrossValidator extends Estimator (
   modelSpecs: Array[ModelSpec],
   evaluator: Evaluator,
   params: ParamMap,
   data: DataFrame) {
+  
+  private val cv = new SparkCrossValidator()
 
-  def cv() { // TODO: need to figure out what the output will be
+  def setEstimator(estimator: Estimator) =
+    cv.setEstimator(estimator.getSparkEstimator)
+  override def getSparkEstimator = cv.getEstimator
+
+  def setEvaluator(evaluator: Evaluator) = {
+    // TODO(hsubrama): Add this as an evaluator of the cross validator.
+    cv.setEvaluator(new BinaryClassificationEvaluator())
+  }
+
+  override def fit(modelSpecs: Array[ModelSpec],
+    data: DataFrame,
+    params: ParamMap): Array[Model] = {
     // split the data based on number of folds
     // TODO: how to represent splits of the data
 
     // train models on each fold
 
+    val cvModel = cv.fit(data)
     // evaluate on the respective folds
 
     // average and return
+
+    //TODO(hsubrama): Build the model class and add the cvModel above into the model.
+    val convertCvModelToModels: Array[Model] = Array()
+    return convertCvModelToModels
   }
 }
+
