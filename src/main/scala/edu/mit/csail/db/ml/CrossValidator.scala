1dd6a77c907888a37355c5b6c934c6e77bb7ab84
kathrynsiegel <ksiegel@mit.edu>
2015-10-20 22:46:40 -0400

    updated cv -- should we reuse more existing Spark classes?

--- src/main/scala/edu/mit/csail/db/ml/CrossValidator.scala
+++ src/main/scala/edu/mit/csail/db/ml/CrossValidator.scala
@@ -3,47 +3,62 @@ package edu.mit.csail.db.ml
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.sql.DataFrame
 import edu.mit.csail.db.ml.{Model, ModelSpec, Evaluator}
-import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
-import org.apache.spark.ml.tuning.{CrossValidator => SparkCrossValidator}
 
 /**
  * This class is responsible for performing cross validation given a set of parameters
  * and models
  * Created by mvartak on 10/14/15.
  */
-class CrossValidator extends Estimator (
-  modelSpecs: Array[ModelSpec],
-  evaluator: Evaluator,
-  params: ParamMap,
-  data: DataFrame) {
+class CrossValidator (
+  myModelSpecs: Array[ModelSpec],
+  myEvaluator: Evaluator,
+  myParams: ParamMap,
+  myData: DataFrame) {
+
+  var modelSpecs: Array[ModelSpec] = myModelSpecs
+  var evaluator: Evaluator = myEvaluator
+  var params: ParamMap = myParams
+  var data: DataFrame = myData
+
+  var dataFolds: Array[DataFrame] = splitData(data, params.get("folds"))
+  var estimator: Estimator = new LREstimator()
+  var db: ModelDB = new ModelDB()
   
-  private val cv = new SparkCrossValidator()
+  // TODO do we want to use existing classes?
+  // private val cv = new SparkCrossValidator()
+  // def setEstimator(estimator: Estimator) =
+  // cv.setEstimator(estimator.getSparkEstimator)
+  // override def getSparkEstimator = cv.getEstimator
+  // def setEvaluator(evaluator: Evaluator) = {
+  //   // TODO(hsubrama): Add this as an evaluator of the cross validator.
+  //   cv.setEvaluator(new BinaryClassificationEvaluator())
+  // }
 
-  def setEstimator(estimator: Estimator) =
-    cv.setEstimator(estimator.getSparkEstimator)
-  override def getSparkEstimator = cv.getEstimator
+  private def splitData(df: DataFrame, param: Param): Array[DataFrame] =
+    // create new dataframe array based on number of folds & instantiate dataframes
+  // loop through features in dataframe and insert into dataframes
+  // return dataframe array
+  Array(new DataFrame())
 
-  def setEvaluator(evaluator: Evaluator) = {
-    // TODO(hsubrama): Add this as an evaluator of the cross validator.
-    cv.setEvaluator(new BinaryClassificationEvaluator())
-  }
-
-  override def fit(modelSpecs: Array[ModelSpec],
-    data: DataFrame,
-    params: ParamMap): Array[Model] = {
-    // split the data based on number of folds
-    // TODO: how to represent splits of the data
-
-    // train models on each fold
-
-    val cvModel = cv.fit(data)
-    // evaluate on the respective folds
-
-    // average and return
-
-    //TODO(hsubrama): Build the model class and add the cvModel above into the model.
-    val convertCvModelToModels: Array[Model] = Array()
-    return convertCvModelToModels
+  // currently will return a Result object holding the info we need
+  // see Result class
+  def run(): Array[Result] = {
+    var results:Array[Result] = empty();
+    modelSpecs foreach (modelSpec => {
+      dataFolds foreach (dataFold => {
+        // TODO(Katie) figure out more about datafolds
+        var dataFoldA = randSelect(dataFold, 0.8);
+        var dataFoldB = dataFold.subtract(dataFoldA); //TODO figure out function that does this
+        var inputA = dataFoldA.get("feature");
+        var expectedA = dataFoldA.get("label");
+        var inputB = dataFoldB.get("feature");
+        var expectedB = dataFoldB.get("label");
+        var model:Model = estimator.fit(modelSpec, inputA, expectedA, params);
+        var res = Array(evaluator.eval(model.predict(inputB), expectedB));
+        results = concat(results, res);
+        db.update(model);
+      })
+    })
+    return results;
   }
 }
-
