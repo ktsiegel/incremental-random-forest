d96ecf91a105408db1ce9dff9897500a69f6d86c
lot9 <lordofthenines@gmail.com>
2015-10-23 00:45:19 -0400

    combined lr logic into 1 file; added build file; fixed compiler errors

--- src/main/scala/edu/mit/csail/db/ml/CrossValidator.scala
+++ src/main/scala/edu/mit/csail/db/ml/CrossValidator.scala
@@ -1,8 +1,12 @@
 package edu.mit.csail.db.ml
 
-import org.apache.spark.ml.param.ParamMap
+// TODO - I had a look at this class in Spark and it’s private to the ml package. We’ll have to hack in something else.
+//import org.apache.spark.ml.param.ParamMap
+
 import org.apache.spark.sql.DataFrame
-import edu.mit.csail.db.ml.{Model, ModelSpec, Evaluator}
+
+// NOTE - At the moment, the following imports are permanently hidden by definitions in package ml
+//import edu.mit.csail.db.ml.{Model, ModelSpec, Evaluator}
 
 /**
  * This class is responsible for performing cross validation given a set of parameters
@@ -10,43 +14,53 @@ import edu.mit.csail.db.ml.{Model, ModelSpec, Evaluator}
  * Created by mvartak on 10/14/15.
  */
 class CrossValidator (
-  modelSpecs: Array[ModelSpec],
-  evaluator: Evaluator,
-  params: ParamMap,
+  modelSpecs: Array[edu.mit.csail.db.ml.ModelSpec],
+  evaluator: edu.mit.csail.db.ml.Evaluator,
+  params: Array[Int],
   data: DataFrame,
-  estimator: Estimator) {
+  estimator: Estimator[edu.mit.csail.db.ml.Model]) {
 
-  var dataFolds: Array[DataFrame] = splitData(data, params.get("folds"))
-  var db: ModelDB = new ModelDB()
+    var dataFolds: Array[DataFrame] = splitData(data, params(0))
+    var db: ModelDB = new ModelDB()
   
-  // TODO do we want to use existing classes?
-  // private val cv = new SparkCrossValidator()
-  // cv.setEstimator(estimator.getSparkEstimator)
-  // cv.setEvaluator(new BinaryClassificationEvaluator())
+    // TODO do we want to use existing classes?
+    // private val cv = new SparkCrossValidator()
+    // cv.setEstimator(estimator.getSparkEstimator)
+    // cv.setEvaluator(new BinaryClassificationEvaluator())
 
-  private def splitData(df: DataFrame, param: Param): Array[DataFrame] =
     // create new dataframe array based on number of folds & instantiate dataframes
-  // loop through features in dataframe and insert into dataframes
-  // return dataframe array
-  Array(new DataFrame())
+    // loop through features in dataframe and insert into dataframes
+    // return dataframe array
+    private def splitData(df: DataFrame, param: Int): Array[DataFrame] =
+    {
+        return Array[DataFrame]()
+    }
 
-  // currently will return a Result object holding the info we need
-  // see Result class
-  def run(): Array[Result] = {
-    var results:Array[Result] = empty();
-    modelSpecs foreach (modelSpec => {
-      dataFolds foreach (dataFold => {
+    // currently will return a Result object holding the info we need
+    // see Result class
+    def run(): Array[Result] = 
+    {
+      var results:Array[Result] = Array[Result]()
+      modelSpecs foreach (modelSpec => {
+        dataFolds foreach (dataFold => {
         // TODO(Katie) figure out more about datafolds
-        var dataFoldA = randSelect(dataFold, 0.8);
-        var dataFoldB = dataFold.subtract(dataFoldA); //TODO figure out function that does this
-        var inputA = dataFoldA.get("feature");
-        var expectedA = dataFoldA.get("label");
-        var inputB = dataFoldB.get("feature");
-        var expectedB = dataFoldB.get("label");
-        var model:Model = estimator.fit(modelSpec, inputA, expectedA, params);
-        var res = Array(evaluator.eval(model.predict(inputB), expectedB));
-        results = concat(results, res);
-        db.update(model);
+	// NOTE - The stuff commented out below does not compile.
+        //var dataFoldA = randSelect(dataFold, 0.8)
+        //var dataFoldB = dataFold.subtract(dataFoldA) //TODO figure out function that does this
+
+        //var inputA = dataFoldA.get("feature")
+        //var expectedA = dataFoldA.get("label")
+        //var inputB = dataFoldB.get("feature")
+        //var expectedB = dataFoldB.get("label")
+        
+	//var model:edu.mit.csail.db.ml.Model = estimator.fit(modelSpec, inputA, expectedA, params)
+        //var res = Array(evaluator.eval(model.predict(inputB), expectedB))
+        
+	//results :+ res
+        
+	var query = " "
+	// TODO: construct update query with 'model' variable 
+        db.update(query)
       })
     })
     return results;
