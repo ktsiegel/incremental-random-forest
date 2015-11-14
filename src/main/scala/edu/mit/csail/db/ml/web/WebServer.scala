5a55da8b28d55e0b5045fac75f1d2d90d1093564
lot9 <lordofthenines@gmail.com>
2015-11-14 17:16:44 -0500

    web server displays static information about test suite at beginning of run

--- src/main/scala/edu/mit/csail/db/ml/web/WebServer.scala
+++ src/main/scala/edu/mit/csail/db/ml/web/WebServer.scala
@@ -1,10 +1,21 @@
 package org.apache.spark.ml
 
 import java.awt.Desktop;
+import java.io.IOException;
 import java.net.URI;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
 import org.apache.spark.SparkContext
+
+import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.handler.AbstractHandler
+
+import scala.collection.mutable.StringBuilder
+
 
 /* 
 	This class is responsible for producing a web interface for WahooML
@@ -20,7 +31,42 @@ class WebServer(port: Int, context: SparkContext)
 {
   require(port >= 0)
 
+  /* This class serves an HTML page with a simple greeting */
+  class GreetingHandler extends AbstractHandler
+  {
+    @throws(classOf[IOException])
+    @throws(classOf[ServletException])
+    override def handle(target: String, baseRequest: Request,
+                        request: HttpServletRequest,
+                        response: HttpServletResponse): Unit =
+    {
+      response.setContentType("text/html;charset=utf-8")
+      response.setStatus(HttpServletResponse.SC_OK)
+
+      baseRequest.setHandled(true)
+
+      // create HTML response
+      val sb = new StringBuilder()
+      sb.append("<!DOCTYPE html>\n")
+      sb.append("<html>\n")
+      sb.append("  <head>\n")
+      sb.append("    <title>WahooML</title>\n")
+      sb.append("  </head>\n")
+      sb.append("  <body>\n")
+      sb.append("    <h1>" + context.appName + "</h1>\n")
+      sb.append("    <p>id:" + context.applicationId + "<p>\n")
+      sb.append("  </body>\n")
+      sb.append("</html>")
+
+      // serve HTML page
+      val writer = response.getWriter()
+      writer.print(sb.toString)
+    }
+  }
+
+  // -set up web server-
   val server = new Server(port)
+  server.setHandler(new GreetingHandler())
 
   // -automatically start the server and display the web interface in a browser-
   server.start()
