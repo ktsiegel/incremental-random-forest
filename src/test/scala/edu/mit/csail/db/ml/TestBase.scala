64e48bc713f1071b04e6b3c56052933286ca53dd
Pablo Ortiz <lot9@users.noreply.github.com>
2015-11-18 16:23:32 -0500

    re-factored TestBase to adhere to spark code style

--- src/test/scala/edu/mit/csail/db/ml/TestBase.scala
+++ src/test/scala/edu/mit/csail/db/ml/TestBase.scala
@@ -6,9 +6,9 @@ import org.apache.spark.sql.{SQLContext, DataFrame}
 object TestBase {
   private val conf = new SparkConf()
     .setMaster("local[2]")
-	.setAppName("test")
-	.set("spark.eventLog.enabled", "true")
-	.set("spark.eventLog.dir", "log")
+	  .setAppName("test")
+	  .set("spark.eventLog.enabled", "true")
+	  .set("spark.eventLog.dir", "log")
   val sc = new SparkContext(conf)
   val sqlContext = SQLContext.getOrCreate(sc)
   val db = new TestDb(TestConfig.databaseName, TestConfig.modelsCollection)
