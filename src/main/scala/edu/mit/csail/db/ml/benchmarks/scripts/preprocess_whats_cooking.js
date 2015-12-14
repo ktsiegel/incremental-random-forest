739ff8693f70b1317c02dace99ea83a7582cf626
Harihar Subramanyam <hahasubbu@gmail.com>
2015-12-14 12:17:07 -0500

    Changed preprocessing script to support outputting specific # of classes

--- src/main/scala/edu/mit/csail/db/ml/benchmarks/scripts/preprocess_whats_cooking.js
+++ src/main/scala/edu/mit/csail/db/ml/benchmarks/scripts/preprocess_whats_cooking.js
@@ -1,33 +1,76 @@
 /**
+ * REMEMBER TO DO `npm install` first.
+ *
  * Execute this script as follows.
  *
- * node preprocess_whats_cooking.js <path_to_training_json> <num_samples_in_training_set>
+ * node preprocess_whats_cooking.js -i <path_to_input> -c <num_classes> -s <num_samples>
+ * -s = The number of training examples to use.
+ * -c = If num_classes = k, this will only use the top k-1 (in terms of frequency) classes.
+ *  The rest will be thrown into a class called "other".
+ * -i = The path to the input training file.
  *
- * It will process the training json file (get it from the Kaggle competition 
+ * The script will process the training json file (get it from the Kaggle competition 
  * https://www.kaggle.com/c/whats-cooking) and put each training example on a single line so that 
  * Spark's JSON reader can parse it.
  *
- * You can also control the number of samples to use in the training set. I recommend using about 
- * 1000 because Spark seems to be extremely slow and memory intensive on a single-node.
+ * You can control the number of samples in the training set. Use the value "all" if you'd like
+ * to use all the samples. You can also select the number of classes to use.
  *
  * The results will be printed to STDOUT, so make sure to redirect STDOUT to an output file.
  */
+
+// TODO: While this is NOT a problem right now, if you find that this script runs slowly because the
+// dataset is too large. You may want to convert the map/filter/reduce calls into for-loops to reduce
+// the number of scans through the dataset and the number of arrays created in memory.
+
 var fs = require("fs");
+var argv = require("optimist").argv;
 
-var args = process.argv.slice(2)
-var path_to_training_json= args[0];
-
+// Figure out where the input file is and read from it.
+var path_to_training_json = argv.i;
 var train = JSON.parse(fs.readFileSync(path_to_training_json));
 
+// Figure out the number of desired samples.
 var num_samples = train.length;
-if (args.length > 1) {
-  num_samples = args[1];
+if (argv.s) {
+  num_samples = Math.min(parseInt(argv.s, 10), num_samples);
 }
 
+// Construct the (cuisine -> frequency) table.
+var frequency_table = train.map(function(obj) {
+  return obj.cuisine;
+}).reduce(function(prev_dict, curr_cuisine) {
+  if (prev_dict[curr_cuisine] === undefined) {
+    prev_dict[curr_cuisine] = 0;
+  }
+  prev_dict[curr_cuisine]++;
+  return prev_dict;
+}, {});
+
+// Figure out the number of desired classes.
+var num_classes = Object.keys(frequency_table).length;
+if (argv.c) {
+  num_classes = Math.min(parseInt(argv.c, 10), num_classes);
+}
+
+// Choose the top num_classes classes.
+var chosen_classes = Object.keys(frequency_table).map(function(key) {
+  return [key, frequency_table[key]];
+}).sort(function(a, b) {
+  return b[1] - a[1];
+}).slice(0, num_classes - 1).map(function(pair) {
+  return pair[0];
+});
+
+// Preprocess the data.
 var output = train.filter(function(obj, index) {
   return index <= num_samples;
 }).map(function(obj) {
-  return JSON.stringify(obj);
+  var copied = JSON.parse(JSON.stringify(obj));
+  if (chosen_classes.indexOf(copied.cuisine) < 0) {
+    copied.cuisine = "other";
+  }
+  return JSON.stringify(copied);
 }).reduce(function(a, b) {
   return a + "\n" + b;
 });
