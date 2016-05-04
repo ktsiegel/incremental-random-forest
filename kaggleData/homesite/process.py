e940dbfeb1d0ba692b17f86a09a9277b37619deb
kathrynsiegel <ksiegel@mit.edu>
2016-05-04 14:03:18 -0400

    cleaning

--- kaggleData/homesite/process.py
+++
@@ -1,9 +0,0 @@
-import random
-f = open('traincopy.csv', 'r')
-fo = open('trainshort.csv', 'w')
-index = 0
-for line in f:
-    if index == 0:
-        fo.write(line)
-    elif random.random() < 0.1:
-        fo.write(line)
